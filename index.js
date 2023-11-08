254df9d8844fd7241fb69c92f7bd6f70201d7f8a
CaveManEN <ianccody@gmail.com>
2023-11-08 17:07:17 -0600

    updated all files

--- index.js
+++ index.js
@@ -1,67 +1,70 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
-const path = require('path'); // Import the 'path' module
+const fs = require('fs').promises; // Use the promise-based version of the fs module
+const path = require('path');
 const { Triangle, Circle, Square } = require('./lib/shapes');
 
 async function generateLogo() {
-  const userInput = await inquirer.prompt([
-    {
-      type: 'input',
-      name: 'text',
-      message: 'Enter up to three characters for the text:',
-    },
-    {
-      type: 'input',
-      name: 'textColor',
-      message: 'Enter text color (keyword or hexadecimal):',
-    },
-    {
-      type: 'list',
-      name: 'shape',
-      message: 'Choose a shape:',
-      choices: ['circle', 'triangle', 'square'],
-    },
-    {
-      type: 'input',
-      name: 'shapeColor',
-      message: 'Enter shape color (keyword or hexadecimal):',
-    },
-  ]);
+  try {
+    const userInput = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for the text:',
+        validate: input => input.length <= 3 || 'Text must be up to three characters long.'
+      },
+      {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter text color (keyword or hexadecimal):',
+        validate: input => /^#?[0-9A-Fa-f]{6}$|^#?[0-9A-Fa-f]{3}$|^[a-zA-Z]+$/.test(input) || 'Please enter a valid color.'
+      },
+      {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        choices: ['circle', 'triangle', 'square'],
+      },
+      {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter shape color (keyword or hexadecimal):',
+        validate: input => /^#?[0-9A-Fa-f]{6}$|^#?[0-9A-Fa-f]{3}$|^[a-zA-Z]+$/.test(input) || 'Please enter a valid color.'
+      },
+    ]);
 
-  const { text, textColor, shape, shapeColor } = userInput;
+    const { text, textColor, shape, shapeColor } = userInput;
 
-  let selectedShape;
-  switch (shape.toLowerCase()) {
-    case 'circle':
-      selectedShape = new Circle();
-      break;
-    case 'triangle':
-      selectedShape = new Triangle();
-      break;
-    case 'square':
-      selectedShape = new Square();
-      break;
-    default:
-      console.error('Invalid shape choice');
-      return;
+    let selectedShape;
+    switch (shape.toLowerCase()) {
+      case 'circle':
+        selectedShape = new Circle();
+        break;
+      case 'triangle':
+        selectedShape = new Triangle();
+        break;
+      case 'square':
+        selectedShape = new Square();
+        break;
+      default:
+        throw new Error('Invalid shape choice');
+    }
+
+    selectedShape.setColor(shapeColor);
+    const svgContent = selectedShape.render(text, textColor);
+
+    const examplesDirectory = path.join(__dirname, 'examples');
+    await fs.mkdir(examplesDirectory, { recursive: true }); // Create the directory if it doesn't exist
+
+    const logoFilePath = path.join(examplesDirectory, 'logo.svg');
+    await fs.writeFile(logoFilePath, svgContent);
+
+    console.log(`Generated logo.svg in the examples directory: ${logoFilePath}`);
+  } catch (error) {
+    console.error('An error occurred:', error.message);
   }
-
-  selectedShape.setColor(shapeColor);
-
-  const svgContent = selectedShape.render(text, textColor);
-
-  // Define the path to the "examples" directory
-  const examplesDirectory = path.join(__dirname, 'examples'); 
-
-  // Specify the full path to the "logo.svg" file within the "examples" directory
-  const logoFilePath = path.join(examplesDirectory, 'logo.svg');
-
-  // Write the SVG file to the "examples" directory
-  fs.writeFileSync(logoFilePath, svgContent);
-
-  console.log('Generated logo.svg in the examples directory');
 }
 
 generateLogo();
+
 
 
